146986f5a778e852ebc955b7ef79e220a3a8e5d5
brensch <brendolini@protonmail.com>
2022-03-03 01:52:30 -0800

    fix #52: added sensor for running in cloud run to set debug off

--- api/main.go
+++ api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -10,13 +9,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var (
-	port = "8080"
-)
+const (
+	// cloud run specific env vars
+	// the vendor lock in is real
+	EnvVarService       = "K_SERVICE"
+	EnvVarRevision      = "K_REVISION"
+	EnvVarConfiguration = "K_CONFIGURATION"
+	EnvVarPort          = "PORT"
 
-func init() {
-	flag.StringVar(&port, "port", port, "port to listen for games on")
-}
+	defaultPort = "8080"
+)
 
 type apiStore struct {
 	log logrus.FieldLogger
@@ -24,16 +26,44 @@ type apiStore struct {
 
 func main() {
 
+	// using env var since cloud run uses it
+	port := os.Getenv(EnvVarPort)
+	if port == "" {
+		port = defaultPort
+	}
+
 	log := logrus.New()
-	// log.SetLevel(logrus.DebugLevel)
+	log.SetLevel(logrus.DebugLevel)
+	log.SetFormatter(&logrus.JSONFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  time.RFC3339Nano,
+	})
 	s := &apiStore{
 		log: log,
+	}
+
+	service := os.Getenv(EnvVarService)
+	revision := os.Getenv(EnvVarRevision)
+	configuration := os.Getenv(EnvVarConfiguration)
+
+	// service being set indicates we're running on cloud run
+	if service != "" {
+		gin.SetMode(gin.ReleaseMode)
+		log.SetLevel(logrus.InfoLevel)
 	}
 
 	r := gin.New()
 	r.Use(MiddlewareLogger(log))
 	api := r.Group("/api")
 	api.POST("/match", s.handleStartMatch)
+
+	log.
+		WithFields(logrus.Fields{
+			"service":       service,
+			"revision":      revision,
+			"configuration": configuration,
+		}).
+		Info("app starting")
 
 	r.Run(fmt.Sprintf(":%s", port))
 
@@ -60,15 +90,15 @@ func MiddlewareLogger(log *logrus.Logger) gin.HandlerFunc {
 		}
 
 		entry := logrus.NewEntry(log).WithFields(logrus.Fields{
-			"hostname":   hostname,
-			"statusCode": statusCode,
-			"latency":    latency, // time to process
-			"clientIP":   clientIP,
-			"method":     c.Request.Method,
-			"path":       path,
-			"referer":    referer,
-			"dataLength": dataLength,
-			"userAgent":  clientUserAgent,
+			"hostname":    hostname,
+			"status_code": statusCode,
+			"latency":     latency, // time to process
+			"client_ip":   clientIP,
+			"method":      c.Request.Method,
+			"path":        path,
+			"referer":     referer,
+			"data_length": dataLength,
+			"user_agent":  clientUserAgent,
 		})
 
 		if len(c.Errors) > 0 {
